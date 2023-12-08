34bc1a40401b4f3b5880992993a6b34c64f071d1
Ngwendu <gambungwendu@gmail.com>
2023-12-08 07:38:03 +0200

    updated index & scripts

--- scripts.js
+++ scripts.js
@@ -1,21 +1,39 @@
-const addBtn = document.querySelector("#add-btn")
-const newTaskInput = document.querySelector('#wrapper input')
-const tasksContainer = document.querySelector('#tasks')
-const error = document.getElementById(error)
-const countValue = document.querySelector('.count-value')
-let taskCount = 0
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+let taskCount = 0;
 
 const displayCount = (taskCount) => {
-    countValue.innerText = taskCount
-}
+  countValue.innerText = taskCount;
+};
 
 const addTask = () => {
-    const taskName = newTaskInput.value.trim()
-    error.style.display = "none"
-    if(!taskName){
-        setTimeout(() => {
-            error.style.display = "block"
-        }, 200)
-        return
-    }
-}
+  const taskName = newTaskInput.value.trim();
+  error.style.display = "none";
+
+  if (!taskName) {
+    setTimeout(() => {
+      error.style.display = "block";
+    }, 200);
+    return;
+  }
+
+  // Create a new task element
+  const newTaskElement = document.createElement("div");
+  newTaskElement.innerText = taskName;
+
+  // Append the new task element to the tasksContainer
+  tasksContainer.appendChild(newTaskElement);
+
+  // Increment taskCount and update display
+  taskCount++;
+  displayCount(taskCount);
+
+  // Clear the input field
+  newTaskInput.value = "";
+};
+
+// Event listener for the "Add" button
+addBtn.addEventListener("click", addTask);
